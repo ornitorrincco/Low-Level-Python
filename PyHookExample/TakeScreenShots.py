cdd1e69ad1e222c5361c5ca9bd5309484775c8c7
ornitorrincco <ornitorrincco@gmail.com>
2019-06-05 23:12:47 -0500

    fix miss spelling error

--- PyHookExample/TakeScreenShots.py
+++ PyHookExample/TakeScreenShots.py
@@ -15,7 +15,7 @@ top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
 
 # create a device context
 desktop_dc = win32gui.GetWindowDC(hdesktop)
-img.dc = win32ui.CreateDCFromHandle(desktop_dc)
+img_dc = win32ui.CreateDCFromHandle(desktop_dc)
 
 # create a memory based device context
 mem_dc = img_dc.CreateCompatibleDC()
@@ -28,7 +28,7 @@ mem_dc.SelectObject(screenShot)
 # copy the screen into our memory device context
 mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
 # save the bitmap to a file
-screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+screenShot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
 # free our objects
 mem_dc.DeleteDC()
-win32gui.DeleteObject(screenshot.GetHandle())
+win32gui.DeleteObject(screenShot.GetHandle())
